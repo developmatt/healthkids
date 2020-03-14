853af770a3a51090b89d841dad0de56b0151b136
Matheus Godoi <matheus.godoi@vianetbrasil.com.br>
2020-03-14 15:06:59 -0300

    Adicionando carousel formstone

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-import $ from 'jquery'
+var $ = require('jquery')
+require('formstone/dist/js/core.js')
+require('formstone/dist/js/mediaquery.js')
+require('formstone/dist/js/touch.js')
+require('formstone/dist/js/carousel.js')
 
+$(".carousel").carousel({
+    paged: true,
+    show: 2,
+    autoAdvance: true,
+    autoTime: 1000,
+    pagination: false,
+    controls: {
+        "container": ".controls",
+        "previous": ".previous",
+        "next": ".next"
+    },
+});
