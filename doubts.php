14ae072f81308ed7a048d9b27537ad21bc52376f
Matheus Godoi <matheusasdev@gmail.com>
2020-06-03 19:41:22 -0300

    Componentizando o doubt_label

--- doubts.php
+++ doubts.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dúvidas frequentes</title>
+<?php require('./components/header.php'); ?>
 
-    <link rel="stylesheet" href="assets/bundle.css" />
-</head>
 <body class="doubts">
     <section id="main" class="main">
         <?php require('./components/internal_menu.php'); ?>
@@ -18,20 +11,36 @@
         <h3 class="doubts__description">Tire todas suas dúvidas sobre o Health Kids:</h3>
 
         <div class="doubts__container">
-            <div class="doubts__doubt doubt closed">
-                <span class="doubt__label">
-                    <span class="doubt__title">O titulo da dúvida</span>
-                    <i class="fas fa-chevron-down icon-closed"></i>
-                    <i class="fas fa-chevron-up icon-opened"></i>
-                </span>
-                <span class="doubt__description-body">
-                    <span class="doubt__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum ducimus aliquid. Recusandae, non debitis id doloremque placeat deserunt, minima incidunt sint similique ipsa amet perspiciatis ad quae, explicabo et?</span>
-                </span>
-            </div>
+            <?php
+                $doubts = [
+                    [
+                        'title' => 'Título da dúvida 1',
+                        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum ducimus aliquid. Recusandae, non debitis id doloremque placeat deserunt, minima incidunt sint similique ipsa amet perspiciatis ad quae, explicabo et?'
+                    ],
+
+                    [
+                        'title' => 'Título da dúvida 2',
+                        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum ducimus aliquid. Recusandae, non debitis id doloremque placeat deserunt, minima incidunt sint similique ipsa amet perspiciatis ad quae, explicabo et?'
+                    ],
+
+                    [
+                        'title' => 'Título da dúvida 3',
+                        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum ducimus aliquid. Recusandae, non debitis id doloremque placeat deserunt, minima incidunt sint similique ipsa amet perspiciatis ad quae, explicabo et?'
+                    ],
+
+                    [
+                        'title' => 'Título da dúvida 4',
+                        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ipsum ducimus aliquid. Recusandae, non debitis id doloremque placeat deserunt, minima incidunt sint similique ipsa amet perspiciatis ad quae, explicabo et?'
+                    ],
+                ];
+
+                foreach($doubts as $doubt) {
+                    require('./components/doubt_label.php');
+                } ?>
         </div>
     </section>
-    <footer>
-        <script src="assets/bundle.js"></script>
-    </footer>
+    
+    <?php require('./components/footer.php'); ?>
+    
 </body>
 </html>
