005ab9043305140803f544c92f5935935284bc7a
Matheus Godoi <matheusasdev@gmail.com>
2020-06-03 13:01:11 -0300

    Iniciando o menu interno

---
+++ components/internal_menu.php
@@ -0,0 +1,14 @@
+<div id="menu__bar" class="menu__bar">
+    <div class="menu__wrapper wrapper">
+        <h1><img src="/images/logo-medium.png" alt="" id="menu__logo" class="menu__logo"></h1>
+
+        <nav id="menu" class="menu">
+            <ul class="menu__list">
+                <li class="menu__item"><a href="#" class="menu__item-link">Benefícios</a></li>
+                <li class="menu__item"><a href="#" class="menu__item-link">Depoimentos</a></li>
+                <li class="menu__item"><a href="#" class="menu__item-link">Como funciona</a></li>
+                <li class="menu__item"><a href="#" class="menu__item-link btn btn-purple">Ver preços</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
