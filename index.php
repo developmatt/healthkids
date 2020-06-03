ad95e53751960aff16e010ccdb49a063b9d88f24
Matheus Godoi <matheusasdev@gmail.com>
2020-06-03 19:26:46 -0300

    Componentizando header e footer

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-
-  <link rel="stylesheet" href="assets/bundle.css" />
-</head>
+<?php require('./components/header.php'); ?>
 
 <body class="index">
   <section id="main" class="main">
@@ -402,86 +393,7 @@
     </div>
   </section>
 
-  <footer>
-
-    <section id="footer" class="footer">
-      <div class="wrapper footer__wrapper">
-
-        <div class="footer__content">
-          <div class="footer__block footer__block--logo">
-            <img src="/images/logo-medium.png" alt="" class="footer__logo">
-          </div>
-          <div class="footer__block footer__block--social footer-social__block">
-            <span class="footer-social__title">REDES SOCIAIS</span>
-
-            <ul class="footer-social__ul">
-              <li class="footer-social__li">
-                <a href="#" class="footer-social__a">
-                  <span class="footer-social__icon-block">
-                    <i class="fab fa-facebook-square"></i>
-                  </span>
-                  <span class="footer-social__text">/healthkids</span>
-                </a>
-              </li>
-
-              <li class="footer-social__li">
-                <a href="#" class="footer-social__a">
-                  <span class="footer-social__icon-block">
-                    <i class="fab fa-instagram"></i>
-                  </span>
-                  <span class="footer-social__text">@healthkids</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="footer__block footer__block--contact">
-            <span class="footer-social__title">SEGUNDA A SEXTA - 8H ÀS 17H</span>
-
-            <ul class="footer-social__ul">
-              <li class="footer-social__li">
-                <a href="#" class="footer-social__a">
-                  <span class="footer-social__icon-block">
-                    <i class="fas fa-phone-volume"></i>
-                  </span>
-                  <span class="footer-social__text">0800 000 0000</span>
-                </a>
-              </li>
-
-              <li class="footer-social__li">
-                <a href="#" class="footer-social__a">
-                  <span class="footer-social__icon-block">
-                    <i class="fab fa-whatsapp"></i>
-                  </span>
-                  <span class="footer-social__text">99999-9999</span>
-                </a>
-              </li>
-
-              <li class="footer-social__li">
-                <a href="#" class="footer-social__a">
-                  <span class="footer-social__icon-block">
-                    <i class="far fa-envelope"></i>
-                  </span>
-                  <span class="footer-social__text">contato@healthkids.com.br</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="footer__nav">
-          <ul class="footer__ul">
-            <li class="footer__li"><a href="#" class="footer__li-link">DÚVIDAS FREQUENTES</a></li>
-            <li class="footer__li"><a href="#" class="footer__li-link">COMPOSIÇÃO</a></li>
-            <li class="footer__li"><a href="#" class="footer__li-link">TERMOS E CONDIÇÕES</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="footer__cnpj">CNPJ: 00.000.000/0001-00 • HEALTH KIDS, BELO HORIZONTE-MG</div>
-    </section>
-
-    <script src="./assets/bundle.js"></script>
-  </footer>
+  <?php require('./components/footer.php'); ?>
 
 </body>
 
