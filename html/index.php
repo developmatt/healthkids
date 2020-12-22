577c4140d5948c59ef722df610838023175a7d5e
Matheus Godoi <matheusasdev@gmail.com>
2020-12-21 22:30:10 -0300

    Modificando forma de carregar video youtube

--- html/index.php
+++ html/index.php
@@ -50,7 +50,11 @@
   <section id="video" class="video">
     <div class="wrapper">
       <div class="video-container">
-        <iframe width="600" height="338" src="https://www.youtube.com/embed/fNmxNS25Wf4" frameborder="0" allow="picture-in-picture" allowfullscreen></iframe>
+        <div class="youtube-video-place embed-responsive embed-responsive-4by3 " data-yt-url="https://www.youtube.com/embed/fNmxNS25Wf4?rel=0&showinfo=0&autoplay=0">
+          <img src="/images/video-thumbnail.png" async class="play-youtube-video">
+        </div>
+
+        <!-- <iframe width="600" height="338" src="https://www.youtube.com/embed/fNmxNS25Wf4" frameborder="0" allow="picture-in-picture" allowfullscreen></iframe> -->
       </div>
 
       <div class="text-container">
